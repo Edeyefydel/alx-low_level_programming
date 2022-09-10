5439a67dc114cadae9656ed9b71d91600e307493
edeyefydel <edeyefydel@gmail.come>
2022-09-09 18:33:51 -0700

    to print alphabet in reversal

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Prints the alphabet at reverse
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
