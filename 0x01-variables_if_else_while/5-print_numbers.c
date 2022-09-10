6543a81e5494e27a5aee7c2c06aefadb09cf5207
edeyefydel <edeyefydel@gmail.come>
2022-09-09 18:01:49 -0700

    print numbers lass than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Prints the numbers from 0 to 9
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < '10'; num ++)
+	{
+		purchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
