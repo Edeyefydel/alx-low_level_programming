846efd6ce2a8bb03ec93759a5cccb7b15d7e6bfc
edeyefydel <edeyefydel@gmail.come>
2022-09-22 18:18:36 -0700

    completed

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+{
+	int n;
+	int b[5];
+	int *p;
+
+	int *p;
+	p = &n;
+	/**
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 *   - you are not allowed to modify p
+	 *   - only one statement
+	 *   - you are not allowed to code anything else than this line of code
+	 */
+	p[5] = 98;
+	/*...so that this prints 98\n */
+	printf("b[2] = %d\n", b[2]);
+	return (0);
+}
