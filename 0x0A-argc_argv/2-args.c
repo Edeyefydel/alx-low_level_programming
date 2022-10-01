d12c116f67e8386a6e0e0c53f581288334bd2348
edeyefydel <edeyefydel@gmail.come>
2022-09-30 18:39:23 -0700

    argument count

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * main - Print the name of the program
+ * @argc: Count arguments
+ *@argv: Arguments
+ *Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
