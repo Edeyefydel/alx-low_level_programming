5084d00fd8ac9daf2560f6cc6f414e75c207cc28
edeyefydel <edeyefydel@gmail.come>
2022-10-10 20:57:17 -0700

    structures and typeof

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *
- *  * struct dog - Struct dog
- *
- *   * @name: name
- *
- *    * @age: age
- *
- *     * @owner: owner
- *
- *      */
-
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 struct dog
 {
 	char *name;
-
 	float age;
-
 	char *owner;
 };
 
